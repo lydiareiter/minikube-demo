7791101bac7446edb0fdff0c94c8bba2e3f477db
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2024-02-16 09:11:15 +0100

    init

---
+++ src/test/java/at/htl/minicube/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package at.htl.minicube;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
